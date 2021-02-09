bc05c477f8b3a45fe81e7dd1f35f2ad3fb778fc3
SabMc <74684143+SabMc@users.noreply.github.com>
2021-02-09 20:59:32 +0100

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Weather app for sailing</p>
-      <footer>
-        <a
-          className="App-link"
-          href="https://github.com/SabMc/sailing-weather-react"
-          target="_blank"
-        >
-          Open-source code
-        </a>{" "}
-        by Sabrina McEwan
-      </footer>
+      <div className="container">
+        <p>Weather app for sailing</p>
+        <footer>
+          <a
+            className="App-link"
+            href="https://github.com/SabMc/sailing-weather-react"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Sabrina McEwan
+        </footer>
+      </div>
     </div>
   );
 }
