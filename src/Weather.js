95135040f355dfc2793cb3d64739b00c7c8ba3ec
SabMc <74684143+SabMc@users.noreply.github.com>
2021-02-10 14:44:07 +0100

    Adding CSS

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,41 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="CurrentCondition">
+    <div className="Weather">
       <div className="SearchForm">
-        <form className="search-engine">
-          <input type="search" placeholder="Sailing Destination" />
-          <input type="submit" className="Search" value="Fair Wind" />
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Sailing Destination"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                className="Search w-100 btn btn-light"
+                value="Fair Wind"
+              />
+            </div>
+          </div>
         </form>
       </div>
-      <h1>Amsterdam</h1>
+      <h2>Amsterdam</h2>
       <div className="row">
-        <div className="col-6">
+        <div className="col-4">
           <ul>
             <li>Wednesday | 10 Feb</li>
             <li>09:48</li>
           </ul>
         </div>
-        <div className="col-6">
+        <div className="col-4">
+          <img src="./Sun1.png" />
+        </div>
+        <div className="col-4">
           <ul>
             <li>xx | xx °C</li>
             <li>Humidity 30%</li>
@@ -25,12 +43,12 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      <h2>Beaufort 3 Gentle Breeze</h2>
+      <h3>Beaufort 3 Gentle Breeze</h3>
       <div className="row">
         <div className="col-6">
           <ul>
-            <li>Wind 12 km/h</li>
-            <li>Gust 25 km/h</li>
+            <li>Wind 12 knots</li>
+            <li>Gust 25 knots</li>
             <li>SSE</li>
             <li>Visibility 6 km</li>
           </ul>
